745730ab099def0f88b24afe32cbf7cb26c3bff9
Myroslava Nezbrytska <miroslavanezbricka@gmail.com>
2024-07-25 15:03:19 +0300

    fixed solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 class Car:
-    def __init__(self, comfort_class: int,
-                 clean_mark: int,
-                 brand: str) -> None:
+    def __init__(self,
+                 comfort_class: int = 1,
+                 clean_mark: int = 1,
+                 brand: str = None) -> None:
         if not (1 <= comfort_class <= 7):
             raise ValueError("Comfort class must be between 1 and 7")
         if not (1 <= clean_mark <= 10):
@@ -13,10 +14,11 @@ class Car:
 
 class CarWashStation:
 
-    def __init__(self, distance_from_city_center: float,
-                 clean_power: int,
-                 average_rating: float,
-                 count_of_ratings: int) -> None:
+    def __init__(self,
+                 distance_from_city_center: float = 1.0,
+                 clean_power: int = None,
+                 average_rating: float = 1.0,
+                 count_of_ratings: int = None) -> None:
         if not (1.0 <= distance_from_city_center <= 10.0):
             raise ValueError("Comfort class must be between 1 and 7")
         if not (1.0 <= average_rating <= 5.0):
@@ -35,18 +37,19 @@ class CarWashStation:
         return round(income, 1)
 
     def calculate_washing_price(self, car: Car) -> float:
-        price = ((car.comfort_class
-                 * (self.clean_power
-                    - car.clean_mark)
-                 * self.average_rating)
-                 / self.distance_from_city_center)
-        return round(price, 1)
+        return round(((car.comfort_class
+                       * (self.clean_power
+                          - car.clean_mark)
+                       * self.average_rating)
+                      / self.distance_from_city_center), 1)
 
     def wash_single_car(self, car: Car) -> None:
         if car.clean_mark < self.clean_power:
             car.clean_mark = self.clean_power
 
     def rate_service(self, rate: int) -> None:
-        total_rating = (self.average_rating * self.count_of_ratings) + rate
+        total_rating = ((self.average_rating
+                         * self.count_of_ratings)
+                        + rate)
         self.count_of_ratings += 1
         self.average_rating = round(total_rating / self.count_of_ratings, 1)
